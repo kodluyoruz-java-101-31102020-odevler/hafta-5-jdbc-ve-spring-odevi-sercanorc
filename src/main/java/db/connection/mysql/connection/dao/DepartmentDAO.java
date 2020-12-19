8aba85d099257a8d3d91a25f7884227a7a7c3ad4
sercanorc <m.sercanoruc@gmail.com>
2020-12-19 17:23:18 +0300

    kodluyoruz

---
+++ src/main/java/db/connection/mysql/connection/dao/DepartmentDAO.java
@@ -0,0 +1,43 @@
+package db.connection.mysql.connection.dao;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import db.connection.mysql.connection.DbSQLQuery;
+import db.connection.mysql.connection.model.Department;
+
+
+public class DepartmentDAO {
+
+	private static final Logger logger = Logger.getLogger(DepartmentDAO.class);
+
+	
+	public List<Department> getAll() {
+		
+		List<Department> departments = new ArrayList<Department>();
+		
+		// Tüm departman listesini çeken SQL komutunu aşağıdaki satıra yazınız.
+		ResultSet resultSet = DbSQLQuery.select("SELECT * FROM departments");
+		
+		try {
+
+			if (resultSet == null) {
+				return departments;
+			}
+			while (resultSet.next()) {
+				Department department = new Department(resultSet.getString("dept_no"), resultSet.getString("dept_name"));
+				departments.add(department);
+			}
+			
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		
+		return departments;
+	}
+	
+}
